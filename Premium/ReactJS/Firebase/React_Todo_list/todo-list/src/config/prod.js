d44ff6e38a7a7cf8c6e4fb4fd2c5ddf326dc20a4
Claude Will <claude.will@yahoo.com>
2020-05-04 23:40:25 -0500

    fix

--- Premium/ReactJS/Firebase/React_Todo_list/todo-list/src/config/prod.js
+++ Premium/ReactJS/Firebase/React_Todo_list/todo-list/src/config/prod.js
@@ -1,10 +1,12 @@
+
+
 export const firebaseConfig = {
-    apiKey: "AIzaSyClNg6MeSLMp7IBK_myLYogRNhB90gXePQ",
-    authDomain: "todo-list-3f74c.firebaseapp.com",
-    databaseURL: "https://todo-list-3f74c.firebaseio.com",
-    projectId: "todo-list-3f74c",
-    storageBucket: "todo-list-3f74c.appspot.com",
-    messagingSenderId: "1075358711640",
-    appId: "1:1075358711640:web:ba850b5c5ddf8d532eef2d",
-    measurementId: "G-3Y0JELM88G"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
   };
